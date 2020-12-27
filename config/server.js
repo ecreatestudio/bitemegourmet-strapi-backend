e68397d424f66ac69ada1edc64160ec6bd269196
Scott Thomson <scott@ecreate.studio>
2020-12-27 14:53:37 +1100

    Initial commit from starter strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '065cefe918993999d7854f54a9d99549'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f3b5ff41-efe3-4084-9c4d-3f449f92c1f4'
+};
